81ab943652400d10c8d649993d9c7a8f03248893
Aiden Bao <aiden@aidens-air.lan>
2024-11-30 20:47:44 -0800

    Use builder to build the airplane.

--- src/src/tutorial/Airplane.java
+++ src/src/tutorial/Airplane.java
@@ -9,12 +9,42 @@ package tutorial;
 public class Airplane {
   public enum Color{RED, BLUE, PURPLE, WHITE, BLACK, GREEN, YELLOW, BROWN, ORANGE}
   public enum EngineType{ELECTRIC, JET, PROPELLER, PAPER, WIND}
+  public static class Builder{
+    public Builder() {
+      this.engineType = EngineType.JET;
+      this.weight = 100000;
+      this.color = Color.WHITE;
+    }
+    public Builder engineType(EngineType type) {
+      this.engineType = type;
+      return this;
+    }
+    public Builder weight(int w) {
+      this.weight = w;
+      return this;
+    }
+    
+    public Builder color(Color c) {
+      this.color = c;
+      return this;
+    }
+
+    public Airplane build() {
+      return new Airplane(this);
+    }
+    protected EngineType engineType;
+    protected int weight;
+    protected Color color;
+  }
   
-  public Airplane(EngineType engineType, int weight, Color color, int speed) {
-    this.engineType = engineType;
-    this.weight = weight;
-    this.color = color;
-    this.speed = speed;
+  public Airplane(Builder builder) {
+    this.engineType = builder.engineType;
+    this.weight = builder.weight;
+    this.color = builder.color;
+    this.speed = 0;
+  }
+  public void speed(int s) {
+    this.speed = s;
   }
   
   public String toString() {
@@ -28,7 +58,8 @@ public class Airplane {
 
 	public static void main(String[] args) {
 	  System.out.println("Airplane is going to lift off...");
-	  Airplane myPlane = new Airplane(EngineType.PAPER, 298742, Color.BLACK, 160);
+	  Airplane myPlane = new Builder().engineType(EngineType.PAPER).weight(90909).color(Color.WHITE).build();
+	  myPlane.speed(234);
 	  System.out.println("My plane is " + myPlane);
 	}
 }
