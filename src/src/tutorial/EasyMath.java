b72a0378abfce9d7357f87f4983cbd269d293861
Aiden Bao <aidenbaowa@gmail.com>
2024-12-01 19:51:46 -0800

    add float double test

--- src/src/tutorial/EasyMath.java
+++ src/src/tutorial/EasyMath.java
@@ -4,8 +4,7 @@ package tutorial;
  * Easy Math for Aiden.
  */
 public class EasyMath {
-
-  public static void main(String[] args) {
+  private static void test1() {
     int var1;
     int var2;
     var1 = 150;
@@ -15,5 +14,21 @@ public class EasyMath {
     System.out.println("product is " + (var1 * var2));
     System.out.println("quotient is " + (var1 / var2));
   }
-
+  
+  private static void test2() {
+    int iresult, irem;
+    double dresult, drem;
+    iresult = 10 / 3;
+    irem = 10 % 3;
+    dresult = 10.0/3.0;
+    drem = 10.0 % 3.0;
+    System.out.println("Result and remainder of 10 / 3: " + iresult + " " + irem);
+    System.out.println("Result and remainder of 10.0 / 3.0: " + dresult + " " + drem);
+  }
+  
+  public static void main(String[] args) {
+    test1();
+    test2();
+  }
 }
+
