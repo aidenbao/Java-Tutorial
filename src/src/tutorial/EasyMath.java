e80cc4d4609332bf7bc4d431d79b367b01330344
Aiden Bao <aidenbaowa@gmail.com>
2024-12-01 19:26:54 -0800

    test your skills - Example 3

---
+++ src/src/tutorial/EasyMath.java
@@ -0,0 +1,19 @@
+package tutorial;
+
+/**
+ * Easy Math for Aiden.
+ */
+public class EasyMath {
+
+  public static void main(String[] args) {
+    int var1;
+    int var2;
+    var1 = 150;
+    var2 = 5;
+    System.out.println("sum is " + (var1 + var2));
+    System.out.println("difference is " + (var1 - var2));
+    System.out.println("product is " + (var1 * var2));
+    System.out.println("quotient is " + (var1 / var2));
+  }
+
+}
